f59799b21f3d35117146f30b036b284540914632
m-idler <marcel.idler@mayflower.de>
2020-07-24 14:54:54 +0200

    refactor ContentView to use PokeCard items

--- PokedexDojo/ContentView.swift
+++ PokedexDojo/ContentView.swift
@@ -9,22 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var pokemon = Pokemon(name: "Pikachu2", number: "025", imagePath: "Pikachu")
+    
     var body: some View {
-        VStack {
-            Text("Pikachu")
-                .font(.title)
-                .fontWeight(.bold)
-            Text("025")
-                .font(.caption)
-            Image("Pikachu")
-                .resizable().aspectRatio(contentMode:.fit)
-                .frame(width: 150.0, height: 150)
+        ScrollView {
+            VStack {
+                PokeCard(pokemon: pokemon)
+                PokeCard(pokemon: pokemon)
+                PokeCard(pokemon: pokemon)
+            }
         }
     }
 }
 
+struct Pokemon {
+    let name: String
+    let number: String
+    let imagePath: String
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            pokemon: Pokemon(name: "Pikachu", number: "025", imagePath: "Pikachu")
+        )
+    }
+}
+
+struct PokeCard: View {
+    let pokemon: Pokemon
+    var body: some View {
+        VStack {
+            Text(pokemon.name)
+                .font(.title)
+                .fontWeight(.bold)
+            Text(pokemon.number)
+                .font(.caption)
+            Image(pokemon.imagePath)
+                .resizable().aspectRatio(contentMode:.fit)
+                .frame(width: 150.0, height: 150)
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(20)
+        .shadow(radius: 10)
     }
 }
